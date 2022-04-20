fe8ed9c002371e67408a33b23933b6486d97e38f
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-20 17:47:21 -0400

    started adding some css

--- src/components/activity.js
+++ src/components/activity.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import "./activity.css";
 
 class RandomList extends React.Component {
   render() {
--- src/components/form.js
+++ src/components/form.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchNewActivity } from "../redux/actionCreators";
+import "./form.css";
 
 class RandomActivityForm extends React.Component {
   render() {
     const { fetchNewActivity } = this.props;
     return (
       <div>
-        <button onClick={fetchNewActivity}>New Activity</button>
+        <button className="button" onClick={fetchNewActivity}>
+          New Activity
+        </button>
       </div>
     );
   }
