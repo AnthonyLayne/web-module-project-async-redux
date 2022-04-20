9dd4c45aea02bdc878f6936efa2de43d9248dc21
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-20 17:08:41 -0400

    started activity component

--- src/components/activity.js
+++ src/components/activity.js
@@ -2,4 +2,11 @@ import React from "react";
 
 import { connect } from "react-redux";
 
-class RandomList extends React.Component {}
+class RandomList extends React.Component {
+  render() {
+    const { activity } = this.props;
+    return <div>{activity}</div>;
+  }
+}
+
+export default connect(null, {})(RandomList);
