5b6e4c0e8b7db97708cdfc34230fe266eac07e97
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-20 16:04:54 -0400

    setting up imports and basics for components

--- src/components/form.js
+++ src/components/form.js
@@ -1,3 +1,6 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { fetchNewActivity } from "../redux/actionCreators";
+
+class RandomActivityForm extends React.Component {}
---
+++ src/redux/reducer.js
@@ -0,0 +1,5 @@
+import { NEW_ACTIVITY } from "./actionTypes";
+
+const initialForm = {};
+
+export default function activityReducer() {}
